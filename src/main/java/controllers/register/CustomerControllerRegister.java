285918c8d42d890ef572ee728cc90d7acac9cfaf
gamballf <gamballf@gmail.com>
2015-03-19 18:42:35 +0100

    Controlador de user con el reconstruct

--- src/main/java/controllers/register/CustomerControllerRegister.java
+++ src/main/java/controllers/register/CustomerControllerRegister.java
@@ -2,13 +2,13 @@ package controllers.register;
 
 
 
-import java.util.Collection;
+
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +22,7 @@ import org.springframework.web.servlet.ModelAndView;
 import controllers.AbstractController;
 import services.CustomerService;
 import domain.Customer;
-import domain.Invoice;
+
 
 
 @Controller
--- src/main/java/controllers/register/UserControllerRegister.java
+++ src/main/java/controllers/register/UserControllerRegister.java
@@ -18,61 +18,114 @@ import services.UserService;
 @RequestMapping("/register/user")
 public class UserControllerRegister extends AbstractController  
 {
-
 	@Autowired
-	UserService userService;
-
-	@RequestMapping("/register")
-	public ModelAndView register() 
-{
-
-	ModelAndView result;
-
-	User user = userService.create();
-	UserForm userForm=new UserForm();
-
-	result = new ModelAndView("register/registerUser");
-	result.addObject("user", userForm);
-
-return result;
-}
+	private UserService userService;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public ModelAndView register() {
+		ModelAndView result;
+		UserForm userForm = new UserForm();
+		result = new ModelAndView("register/user");
+		result.addObject("actor", "user");
+		result.addObject("userForm", userForm);
+		return result;
+	}
+	
 	
 @RequestMapping(value = "/register", method = RequestMethod.POST, params = "save")
-public ModelAndView save(@Valid User user, BindingResult binding) 
+public ModelAndView save(@Valid UserForm userForm,	BindingResult binding) 
 {
+	ModelAndView result;
+	
+		result = new ModelAndView("register/user");
+		result.addObject("userForm", userForm);
+		result.addObject("actor", "user");		
 
-	ModelAndView result = new ModelAndView();
-	if (binding.hasErrors()) {
-		result = createEditModelAndView(user);
-		} else {		
+		User user = userService.reconstruct(userForm);
 		userService.save(user);
-		result = new ModelAndView("redirect:/security/login.do");					
-}
-return result;
-}
-	@RequestMapping("/registerFailure")
-	public ModelAndView failure() 
-{
-	ModelAndView result;
 
-	result = new ModelAndView("redirect:register.do?showError=true");
+		result = new ModelAndView("redirect:../welcome/index.do");
+	
+		return result;
+}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
-return result;
-}
-	protected ModelAndView createEditModelAndView(User user) 
-{
-	ModelAndView result;
-	result = createEditModelAndView(user, null);
-return result;
-}
-	protected ModelAndView createEditModelAndView(User user, String message) 
-{
-
-	ModelAndView result;
-	result = new ModelAndView("register/registerUser");
-	result.addObject("user", user);
-	result.addObject("message", message);
-return result;
-}
-
-}
+//
+//	@Autowired
+//	UserService userService;
+//
+//	@RequestMapping("/register")
+//	public ModelAndView register() 
+//{
+//
+//	ModelAndView result;
+//
+//	User user = userService.create();
+//	UserForm userForm=new UserForm();
+//
+//	result = new ModelAndView("register/registerUser");
+//	result.addObject("user", userForm);
+//
+//return result;
+//}
+//	
+//@RequestMapping(value = "/register", method = RequestMethod.POST, params = "save")
+//public ModelAndView save(@Valid User user, BindingResult binding) 
+//{
+//
+//	ModelAndView result = new ModelAndView();
+//	if (binding.hasErrors()) {
+//		result = createEditModelAndView(user);
+//		} else {		
+//		userService.save(user);
+//		result = new ModelAndView("redirect:/security/login.do");					
+//}
+//return result;
+//}
+//	@RequestMapping("/registerFailure")
+//	public ModelAndView failure() 
+//{
+//	ModelAndView result;
+//
+//	result = new ModelAndView("redirect:register.do?showError=true");
+//
+//return result;
+//}
+//	protected ModelAndView createEditModelAndView(User user) 
+//{
+//	ModelAndView result;
+//	result = createEditModelAndView(user, null);
+//return result;
+//}
+//	protected ModelAndView createEditModelAndView(User user, String message) 
+//{
+//
+//	ModelAndView result;
+//	result = new ModelAndView("register/registerUser");
+//	result.addObject("user", user);
+//	result.addObject("message", message);
+//return result;
+//}
+//
+//}
