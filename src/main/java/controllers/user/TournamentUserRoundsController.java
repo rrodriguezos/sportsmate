34495eba3e61708fc7ea85a9d9b3b1393ef8a841
juanjepl <juanjepl@gmail.com>
2015-04-14 16:01:21 +0200

    import corregido

--- src/main/java/controllers/user/TournamentUserRoundsController.java
+++ src/main/java/controllers/user/TournamentUserRoundsController.java
@@ -5,9 +5,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-import javax.validation.Valid;
-
-import org.jsoup.Connection.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
